5b6300d29e189f6fbcd9fcb4dd388e88a38f8943
Zhenglai Zhang <zhenglai.zhang@quadas.com>
2017-01-16 17:09:18 +0800

    preparing

--- init_env.sh
+++ init_env.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+
+sudo apt-get install python3-tk
+
 pip3 install --upgrade pip
 
-pip3 install numpy scipy maplotlib pandas sklearn
+pip3 install numpy scipy maplotlib pandas scikit-learn
